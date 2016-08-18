35825cecb11bf05ee1ffcd5e116768f677f6a372
naomi <naomi.emma.spencer@gmail.com>
2016-08-18 16:21:41 +0100

    Can report broken bikes

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,9 @@ require "bike"
 
 describe Bike do
   it { is_expected.to respond_to :working? }
+
+  it "It can be reported broken" do
+    subject.report_broken
+    expect(subject).to be_broken
+  end
 end
