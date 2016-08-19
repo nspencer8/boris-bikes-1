7f410708e290283e249e33a146c5895e35b54751
naomi <naomi.emma.spencer@gmail.com>
2016-08-19 12:09:33 +0100

    WIP - Garage Class

---
+++ lib/garage.rb
@@ -0,0 +1,30 @@
+class Garage
+
+def initialize
+  @working_bikes = []
+  @broken_bikes = []
+end
+
+def collect(bikes, target)
+  if target == :working
+    @working_bikes += bikes
+  elsif target == :broken
+    @broken_bikes += bikes
+  else
+    raise 'Check Collect Target'
+  end
+end
+
+def give(destination, target)
+  if target == :working
+    destination.collect(@working_bikes, :working)
+    @working_bikes = []
+  elsif target == :broken
+    destination.collect(@broken_bikes, :broken)
+    @broken_bikes = []
+  else
+    raise 'Check Destination'
+  end
+end
+
+end
